e9bc3a6dd4426ec19787493e0fc3293de155fdb9
vtlnr <149604564+vtlnr@users.noreply.github.com>
2024-04-29 01:28:07 +0300

    b107

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,13 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int c = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
-                c++;
-            }
-        }
-    }
+  for (int i = 0; i < len; i++) {
+      for (int j = i + 1; j < len; j++) {
+          if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
+              c++;
+          }
+      }
+  }
   return c;
 }
 int countPairs2(int *arr, int len, int value) {
